307adbe9676022ddee6c0f405ad8567695276674
zhang yuntian <615787469@qq.com>
2017-04-11 14:02:57 +0800

    Complete the list of cities and counties across the province of three functions

--- app/src/main/java/com/yunyiweather/android/util/HttpUtil.java
+++ app/src/main/java/com/yunyiweather/android/util/HttpUtil.java
@@ -1,5 +1,6 @@
 package com.yunyiweather.android.util;
 
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -11,10 +12,9 @@ import okhttp3.Request;
 
 public class HttpUtil {
 
-    public static void setOkHttpRequest(String address, okhttp3.Callback callback){
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
-        client.newCall(request).enqueue(callback);
+        client.newCall(request).enqueue(callback);//异步请求
     }
-
 }
